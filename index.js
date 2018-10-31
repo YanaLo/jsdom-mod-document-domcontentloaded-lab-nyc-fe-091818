a6076b238cc133d93c716fea26043586694e2c7c
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2018-10-31 05:39:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 /* code goes here */
+// document.addEventListener(DOMContentLoaded)
+  document.addEventListener("DOMContentLoaded", e => {
+      document.querySelector("body").addEventListener("click", e => console.log("Reggae, Reggae!"));
+      })
+      let hiddenName=document.getElementById("hidden-div")
+      hiddenName.innerText="301"
